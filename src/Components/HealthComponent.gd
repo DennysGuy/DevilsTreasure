18c8238faf6a3053b5fe153bd477282a2d9c8ee2
Matt Matriciano <mmatriciano@ewu.edu>
2024-03-18 13:33:50 -0700

    building out hitbox stuff

--- src/Components/HealthComponent.gd
+++ src/Components/HealthComponent.gd
@@ -12,11 +12,11 @@ func _ready():
 func get_health() -> float:
 	return cur_health
 
-func set_heath(health: float):
+func set_heath(health : float):
 	cur_health = health
 
-func apply_damage(attack: Attack):
-	cur_health -= randi_range(attack.min_damage, attack.max_damage)
+func apply_damage(min_damage : int, max_damage : int):
+	cur_health -= randi_range(min_damage, max_damage)
 
 func apply_healing(health: float):
 	if self.cur_health + health > self.max_health:
--- src/Components/HurtBoxComponent.gd
+++ src/Components/HurtBoxComponent.gd
@@ -1,11 +1,22 @@
 extends Area2D
 class_name HurtBox
 
+@export
+var parent : Entity
+@export
+var health_component : HealthComponent
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func bullet_detected(hitbox : HitBox, state : State) -> bool:
+	if self.has_overlapping_areas():
+		hitbox.parent.queue_free()
+		return true
+	
+	return false
