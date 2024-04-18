741e2973b1670adbbaadaf1ad9d9b4dd289aec8a
Matt Matriciano <mmatriciano@ewu.edu>
2024-04-17 21:01:24 -0700

    enemy can patrol

--- src/Entities/Enemies/CompTestEnemy/Idle.gd
+++ src/Entities/Enemies/CompTestEnemy/Idle.gd
@@ -6,13 +6,21 @@ var patrol : State
 @onready
 var timer = $"../../Timer"
 
+@onready
+var VelocityComp = $"../../VelocityComponent"
+
 func enter() -> void:
+	print("I'm in idle state")
 	super()
 	timer.wait_time = 3.0
+	VelocityComp.move_speed = 0
 	timer.start()
 
 func process_physics(_delta: float) -> State:
-	if timer.is_stopped():
+	VelocityComp.apply_horizontal_movement(1)
+	self.parent.velocity.y += gravity * _delta
+	self.parent.move_and_slide()
+	if timer.time_left == 0:
 		return patrol
 	
 	return null
--- src/Entities/Enemies/CompTestEnemy/Patrol.gd
+++ src/Entities/Enemies/CompTestEnemy/Patrol.gd
@@ -7,21 +7,24 @@ var idle : State
 var timer = $"../../Timer"
 
 @onready
-var VelocityComponent = $"../../VelocityComponent"
+var VelocityComp = $"../../VelocityComponent"
 
 var directions : Array = [-1, 1]
 var dir : int
 func enter() -> void:
 	super()
+	VelocityComp.move_speed = 20
 	timer.wait_time = 2.0
 	dir = directions.pick_random()
 	timer.start()
+	print("I'm in patrol state")
 
 func process_physics(_delta: float) -> State:
+	self.parent.velocity.y += gravity * _delta
+	VelocityComp.apply_horizontal_movement(dir)
+	self.parent.move_and_slide()
 	
-	VelocityComponent.apply_horizontal_movement(dir)
-	
-	if timer.is_stopped():
+	if timer.time_left == 0:
 		return idle
 	
 	return null
